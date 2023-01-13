944d00e00b8d452914a2759b9ab456cd1a0e3679
erich <erichpratamasaputra@gmail.com>
2023-01-13 18:56:24 +0800

    [Feature] Add About Fragment

--- app/src/main/java/me/erichps/photogallery/view/AboutFragment.kt
+++ app/src/main/java/me/erichps/photogallery/view/AboutFragment.kt
@@ -1,9 +1,37 @@
 package me.erichps.photogallery.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import me.erichps.photogallery.databinding.FragmentAboutBinding
 
 @AndroidEntryPoint
 class AboutFragment : Fragment() {
+
+    private var binding: FragmentAboutBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            webview.loadUrl("file:///android_res/raw/about.html")
+        }
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
 
 }
